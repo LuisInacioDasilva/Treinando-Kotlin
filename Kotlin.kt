20922fe2a84b7bb2b13caa7a12ba385c8aa53db4
LuisInacioDasilva <luisinacio1996@gmail.com>
2022-04-19 21:21:00 -0300

    Primeiro

---
+++ Kotlin.kt
@@ -0,0 +1,23 @@
+fun main(){
+    
+    //variáveis cujo valor pode mudar a qualquer momento
+    var vmutavel = "Luis"
+    println(vmutavel)
+    
+    var vmutavel2 = " Silva"
+    println(vmutavel2)
+    
+    var vmutavel3 = vmutavel + vmutavel2
+    println(vmutavel3)
+    
+    //variáveis cujo valor não muda, é constante
+    
+    val vimutavel = 3.14
+    //colocar sempre "." ao invés de ","
+    
+    var vmutavel4 = 6
+    
+    var vmutavel5 = vmutavel4 + vimutavel
+    
+   println(vmutavel5)
+}
